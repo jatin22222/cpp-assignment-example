db9b34ac82bc866bf22c48f487c937a91850e1a8
jatin22222 <146483561+jatin22222@users.noreply.github.com>
2024-09-19 23:55:54 +0530

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+// Function prototype
+int factorial(int n);
+
+int main() {
+    int n;
+    std::cout << "Enter a number: ";
+    std::cin >> n;
+    std::cout << "Factorial of " << n << " is " << factorial(n) << std::endl;
+    return 0;
+}
+
+// TODO: Implement the factorial function
+int factorial(int n) {
+    // Implement your code here
+    return 1;
+}
